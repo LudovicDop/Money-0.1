aad5735762e14e0a49269b70df35add15b5b5534
Loacking <LudovicDop@gmail.com>
2022-09-12 21:17:13 +0200

    PATH pour windows et macos

--- Money-0.1/createAccount.c
+++ Money-0.1/createAccount.c
@@ -10,7 +10,8 @@
 #include <string.h>
 #include <time.h>
 #include <assert.h>
-#define PATH "C:/Users/Ludov/Desktop/Money-0.1/Money-0.1/account_file/"
+//#define PATH "C:/Users/Ludov/Desktop/Money-0.1/Money-0.1/account_file/"
+#define PATH "/Users/ludovicdoppler/Desktop/Money-0.1/Money-0.1/account_file/"
 char* concat(const char *s1, const char *s2)
 {
     char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
@@ -107,7 +108,7 @@ void addAmount(char *nameAccount,char *usr,int amountAdd,char *why)
     
     fprintf(fileAccount, cNewWithReturn); //HERE
     
-    fprintf(fileAccount, " =>  %d + %d ( Reason : %s ) %d/%d/%d",x,amountAdd,why,tm_time->tm_mday,tm_time->tm_mon,tm_time->tm_year-100);
+    fprintf(fileAccount, " =>  %d + %d ( Reason : %s ) %d/%d/%d",x,amountAdd,why,tm_time->tm_mday,tm_time->tm_mon + 1,tm_time->tm_year-100);
     printf("         *** %s do you have now %d €***\n\n",usr,result);
 
     fclose(fileAccount);
@@ -123,6 +124,10 @@ char *strremove(char *str, const char *sub) {
         }
     }
     return str;
+}
+
+void month(){
+    
 }
 
 
